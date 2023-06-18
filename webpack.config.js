ffa0196911218ed415c775c796a392cf1889c2e8
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-06-18 19:50:20 +0200

    file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,51 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, "client/src", "main.jsx"),
-    module: {
-        rules: [
+  entry: path.join(__dirname, "client/src", "main.jsx"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|jpg|jpeg)$/,
+        use: [
           {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-            }
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
           },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: path.join(__dirname, "build"),
-        filename: "bundle.js",
-        publicPath: "/"
-    }, 
-    devtool: 'inline-source-map',
-    plugins:[
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebpackPlugin({
-           template: path.join(__dirname,'/client/index.html')
-      }) 
-    ],
-    // devServer: {
-    //     hot: true,
-    //     historyApiFallback: true
-    // }
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/client/index.html')
+    })
+  ],
+  // devServer: {
+  //     hot: true,
+  //     historyApiFallback: true
+  // }
 };
