d9750927988c927c6234b82e1c37ea12a1a96caf
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-06-18 19:39:20 +0200

    app property of Server

--- server/app/Server.js
+++ server/app/Server.js
@@ -1,27 +1,27 @@
+const express = require('express');
+
+
 class Server {
 
   constructor(host, port) {
     this.host = host;
     this.port = port;
+    this.app = express();
   };
 
   start(DIST_DIR, HTML_FILE) {
-    const express = require('express');
-    const app = express();
-
     const mockResponse = {
       foo: 'bar',
       bar: 'foo'
     };
-    app.use(express.static(DIST_DIR));
-    app.get('/api', (req, res) => {
+    this.app.use(express.static(DIST_DIR));
+    this.app.get('/api', (req, res) => {
       res.send(mockResponse);
     });
-    app.get('/', (req, res) => {
+    this.app.get('/', (req, res) => {
       res.sendFile(HTML_FILE);
     });
-
-    app.listen(this.port, () => {
+    this.app.listen(this.port, () => {
       console.log('App listening on port: ' + this.port);
     });
   }
