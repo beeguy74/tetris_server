4ab674e3438fc29cf5a173fab6a56eaa4cc92369
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-06-18 11:28:31 +0200

    server without express

--- server/index.js
+++ server/index.js
@@ -1,16 +1,61 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const mockResponse = {
-  foo: 'bar',
-  bar: 'foo'
-};
-app.get('/api', (req, res) => {
-  res.send(mockResponse);
-});
-app.get('/', (req, res) => {
- res.status(200).send('Hello World from the server!');
-});
-app.listen(port, function () {
- console.log('App listening on port: ' + port);
-});
+import fs  from 'fs'
+import debug from 'debug'
+
+const logerror = debug('tetris:error')
+  , loginfo = debug('tetris:info')
+
+const initApp = (app, params, cb) => {
+  const {host, port} = params
+  const handler = (req, res) => {
+    const file = req.url === '/bundle.js' ? '/../build/bundle.js' : '/../client/index.html'
+    console.log("FILE!", file)
+    fs.readFile(__dirname + file, (err, data) => {
+      if (err) {
+        logerror(err)
+        res.writeHead(500)
+        return res.end('Error loading index.html')
+      }
+      res.writeHead(200)
+      res.end(data)
+    })
+  }
+
+  app.on('request', handler)
+
+  app.listen({host, port}, () =>{
+    loginfo(`tetris listen on ${params.url}`)
+    cb()
+  })
+}
+
+const initEngine = io => {
+  io.on('connection', function(socket){
+    loginfo("Socket connected: " + socket.id)
+    socket.on('action', (action) => {
+      if(action.type === 'server/ping'){
+        socket.emit('action', {type: 'pong'})
+      }
+    })
+  })
+}
+
+export function create(params){
+  const promise = new Promise( (resolve, reject) => {
+    const app = require('http').createServer()
+    initApp(app, params, () =>{
+      const io = require('socket.io')(app)
+      const stop = (cb) => {
+        io.close()
+        app.close( () => {
+          app.unref()
+        })
+        loginfo(`Engine stopped.`)
+        cb()
+      }
+
+      initEngine(io)
+      resolve({stop})
+    })
+  })
+  return promise
+}
--- webpack.server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './client/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-};
