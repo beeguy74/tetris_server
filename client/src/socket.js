ba6389d32a1d4c4550cefb2829f0f4b206a92d7f
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-06-21 16:25:54 +0200

    cors troubles solved

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:3001";
+const URL = "http://0.0.0.0:3001";
 
 export const socket = io(URL);
