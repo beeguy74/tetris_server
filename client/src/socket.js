080fe3763048fe997d3416c5845564d00527a8ad
iaroslav Neverov <beeguy74@gmail.com>
2023-07-07 12:46:05 +0200

    listroom

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,4 +3,6 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = "http://0.0.0.0:3001";
 
+io.on('rooms', (data) => console.log('rooms', data));
+
 export const socket = io(URL);
--- server/app/controllers/roomController.js
+++ server/app/controllers/roomController.js
@@ -3,3 +3,7 @@ exports.newRoom = function newRoom(data) {
     this.dataService.add('rooms', data, this.socket.id);
     this.socket.join('data');
 }
+
+exports.listRooms = function listRooms(data) {
+    this.io.to(this.socket.id).emit('rooms',this.io.sockets.adapter.rooms);
+}
