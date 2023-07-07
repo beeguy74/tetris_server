a84ef7ccf3bb32d46b1dd0b6b8410394c2c89b9e
Olga Kushnirova <olgakushnirova27@gmail.com>
2023-07-07 13:49:22 +0200

    hidden

--- client/src/components/Game/Stage/Stage.styles.js
+++ client/src/components/Game/Stage/Stage.styles.js
@@ -3,8 +3,8 @@ import { STAGE_WIDTH, STAGE_HEIGHT } from '../../../tools/setup';
 
 export const StyledStage = styled.div`
   display: grid;
-  grid-template-columns: repeat(${STAGE_WIDTH}, 40px);
-  grid-template-rows: repeat(${STAGE_HEIGHT}, 40px);
+  grid-template-columns: repeat(${STAGE_WIDTH}, 35px);
+  grid-template-rows: repeat(${STAGE_HEIGHT}, 35px);
   grid-gap: 1px;
   border: 1px solid #777;
   background: #222;
--- client/src/components/SoloGame/SoloGame.styles.js
+++ client/src/components/SoloGame/SoloGame.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledTetrisWrapper = styled.div`
   width: 100%;
   height: 100vh;
-  // overflow: hidden;
+  overflow: hidden;
   outline: none;
 `;
 
@@ -11,7 +11,7 @@ export const StyledTetris = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 40px;
+  padding: 10px;
   margin: 0 auto;
 
   .display {
