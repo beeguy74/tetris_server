8dd8c61a48103e6176772d3e03e3968ec35b30b1
iaroslav Neverov <beeguy74@gmail.com>
2023-07-10 17:20:31 +0200

    rooms bug is healed

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = "http://0.0.0.0:3001";
+const socket = io(URL);
 
-io.on('rooms', (data) => console.log('rooms', data));
+socket.on('rooms', (data) => console.log('rooms', data));
 
-export const socket = io(URL);
+export {socket};
